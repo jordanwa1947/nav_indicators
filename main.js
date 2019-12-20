62adc707c376fbba7f3ee34bf7fb421aa311a761
Jordan Whitten <jordanwa1947@gmail.com>
2019-12-20 10:13:17 -0700

    Removes the selected class from links that werent selected and uses the nodeType attribute to distinguish li elements from text elements

--- main.js
+++ main.js
@@ -3,5 +3,11 @@ var navLinks = document.getElementById('nav-link-list');
 navLinks.addEventListener('click', indicateClick);
 
 function indicateClick() {
+  var links = event.target.parentElement.childNodes;
+  for (var i = 0; i < links.length; i++) {
+    if(links[i].nodeType === 1) {
+      links[i].classList.remove('selected');
+    }
+  }
   event.target.classList.add('selected');
 }
